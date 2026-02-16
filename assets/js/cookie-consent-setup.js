67da2a054bc6d23b0334908bbfec8e94b3d12922
okayu1015 <33214171+okayu1015@users.noreply.github.com>
2026-02-16 05:28:34 +0000

    Deploying to gh-pages from @ okayu1015/profile@4c8d7ffd078bf15329139b0d514f7182198b3444 🚀

--- assets/js/cookie-consent-setup.js
+++ assets/js/cookie-consent-setup.js
@@ -96,7 +96,7 @@ function initializeCookieConsent() {
               },
               {
                 title: 'More information',
-                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://okayu1015.github.io/profile/#contact">contact us</a>.'
+                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://okayu1015.github.io/profile//profile/#contact">contact us</a>.'
               }
             ]
           }
